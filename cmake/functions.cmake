d283d038ffc2ab0cdcd386a2169bc74641d297f0
seanyen <seanyen@microsoft.com>
2020-04-14 17:01:52 -0700

    fix

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -72,6 +72,9 @@ macro(google_initialize_cartographer_project)
     set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH}
         ${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules)
   endif()
+  if(WIN32)
+    return()
+  endif()
   set(GOOG_CXX_FLAGS "-pthread -std=c++11 -fPIC ${GOOG_CXX_FLAGS}")
 
   google_add_flag(GOOG_CXX_FLAGS "-Wall")
